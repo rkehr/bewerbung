2990c526fba5e50f0a074e19e7493e107e767f51
Robin Kehr <ich@robinkehr.de>
2020-11-21 17:40:53 +0100

    moved /pages/Technologien.js to /pages/Skills.js

---
+++ src/pages/Skills.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Page, SkillMeterGroup, SkillGroupFilter } from '../components';
+import { useDataStore } from '../state';
+
+const Skills = ({ page }) => {
+  const skillLevels = useDataStore((state) => state.skillLevels);
+
+  const categories = skillLevels.reduce((acc, group) => {
+    if (!acc.includes(group.category)) {
+      acc.push(group.category);
+    }
+    return acc;
+  }, []);
+  const [categoryFilters, setCategoryFilters] = useState('Alle');
+
+  const filteredSkillLevels = skillLevels.filter((group) => {
+    if (categoryFilters && !categoryFilters.includes('Alle')) {
+      return categoryFilters.includes(group.category);
+    } else {
+      return true;
+    }
+  });
+
+  return (
+    <>
+      <SkillGroupFilter
+        categories={categories}
+        setCategoryFilters={setCategoryFilters}></SkillGroupFilter>
+      <Page page={page} header={true}>
+        <div className='gridContainer'>
+          <SkillMeterGroup
+            groupName='Jack of all trades, master of some...'
+            skillLevels={[]}>
+            <p>
+              Ich lerne leidenschaftlich gerne neue Dinge!
+              <br />
+              Führt das gelegentlich dazu dass ich so viele verschiedene Dinge
+              lerne, dass ich am ende vieles ein bisschen kann und wenig
+              perfekt? Vielleicht.
+            </p>
+            <p>
+              Allerdings hilft es mir in vielen Situationen auch dabei das große
+              Ganze zu sehen und viele Konzepte die in mehreren
+              Programmiersprachen und Programmen auftauchen anwenden zu können
+            </p>
+          </SkillMeterGroup>
+
+          {filteredSkillLevels.map(({ groupName, skillLevels }, index) => {
+            return (
+              <SkillMeterGroup
+                key={index.toString()}
+                groupName={groupName}
+                skillLevels={skillLevels}></SkillMeterGroup>
+            );
+          })}
+        </div>
+      </Page>
+    </>
+  );
+};
+
+Skills.propTypes = {
+  page: PropTypes.object,
+};
+
+export default Skills;
