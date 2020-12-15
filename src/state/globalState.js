7e16c712641c11369927037bc556e37d566b4a35
Robin Kehr <ich@robinkehr.de>
2020-12-15 02:44:03 +0100

    added page slide direction to global state

--- src/state/globalState.js
+++ src/state/globalState.js
@@ -1,8 +1,6 @@
 import create from 'zustand';
 // import produce from 'immer';
 
-import { skillLevels, occupationTimeLine, themes } from '../data';
-
 //const immer = config => (set, get) => config(fn => set(produce(fn), get));
 
 const useGlobalStore = create((set) => ({
@@ -18,5 +16,11 @@ const useGlobalStore = create((set) => ({
         return;
       }
     }),
+  pageDirection: 1,
+  setPageDirection: (direction) => {
+    set(() => {
+      return { pageDirection: direction };
+    });
+  },
 }));
 export { useGlobalStore };
