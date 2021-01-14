49550161e512c66a39dc4bbc21105c817c009eae
Robin Kehr <ich@robinkehr.de>
2021-01-14 03:56:57 +0100

    updated occupation descriptions

--- src/data/occupationTimeLine.js
+++ src/data/occupationTimeLine.js
@@ -3,7 +3,7 @@ const occupationTimeLine = [
     name: 'Internationaler Studiengang Medieninformatik',
     organisation: 'HTW Berlin',
     description:
-      'Seid Einigen Jahren (I know, I know) Studier ich inzwischen Medieninformatik. Für längere Zeit war ich mir nicht sicher ob ich überhaupt fertigstudieren möchte weil mir die kreative Komponente fehlt, jetzt aber habe ich beschlossen das gelernte einfach kreativ anzuwenden.',
+      'Seid Einigen Jahren (I know, I know) Studier ich inzwischen Medieninformatik. Für längere Zeit war ich mir nicht sicher ob ich überhaupt fertigstudieren möchte weil mir die kreative Komponente fehlte, jetzt aber habe ich die Kreativität in der Programmierung entdeckt.',
     interval: {
       start: new Date('2016-4-1'),
       end: new Date(),
@@ -15,7 +15,7 @@ const occupationTimeLine = [
     name: 'Content Creator',
     organisation: 'Urban Motors und Bredlow Automobile via Soniques GmbH',
     description:
-      'Hier habe ich für die Socialmediaauftritte zweier Werkstätten bzw. Autohändler Fotos gemacht und getextet. Weil ich sonst weder viel mit Autos noch mit Motorrädern am Hut hab, war es hier besonders spannend Motive zu suchen und dabei gleich zwei Werkstätten entdecken zu können.',
+      'Hier habe ich für die Socialmediaauftritte zweier Werkstätten bzw. Autohändler Fotos gemacht und getextet. Weil ich sonst weder viel mit Autos noch mit Motorrädern am Hut hab, war es hier besonders spannend gute Motive zu suchen und dabei gleich zwei Werkstätten entdecken zu können.',
     interval: {
       start: new Date('2016-12-1'),
       end: new Date('2017-5-31'),
@@ -39,7 +39,7 @@ const occupationTimeLine = [
     name: 'Webdesign und Development',
     organisation: 'db Automobile GmbH',
     description:
-      "Die Getriebewerkstat brauchte eine Website und ich habe knapp ein halbes Jahr and einer neuen Wordpress Website für sie gebastelt und das Theme 'from scratch' geschrieben.Leider konnten ich und mein damaliger Chef uns nicht darauf einigen wie eine moderne Website aussehen sollte und er ist letzten Endes bei seiner alten geblieben. Leider existieren hiervon nurnoch Fotos und ein Logo dass ich entworfen hatte. Ich habe jedoch trotzdem sehr viel über die Entwicklung von Themes und Plugins für Wordpress gelernt.",
+      "Diese Getriebewerkstat brauchte eine Website und ich habe knapp ein halbes Jahr and einem neuen Wordpresstheme für sie 'from scratch' gebastelt. Leider konnten ich und mein damaliger Chef uns nicht darauf einigen wie eine moderne Website aussehen sollte und er ist letzten Endes bei seiner alten geblieben. Leider existieren hiervon nurnoch Fotos und ein Logo dass ich entworfen hatte. Ich habe jedoch trotzdem sehr viel über die Entwicklung von Themes und Plugins für Wordpress gelernt.",
     interval: {
       start: new Date('2018-12-1'),
       end: new Date('2019-4-30'),
@@ -51,7 +51,7 @@ const occupationTimeLine = [
     name: 'Webentwicklung',
     organisation: 'Cleaner.Earth',
     description:
-      'Mein Herzensprojekt. Mit meinem Vater und meinem Bruder habe ich Cleaner.Earth, eine Online Plattform zur Finanzierung von Plastiksammel- und Recyclingprojekten, gegründet und die Entwicklung der Website übernommen. Nachdem wir anfänglich auch mit Wordpress arbeiteten haben wir uns vor kurzem dazu entschieden die Website als Webapp auf dem MERN Stack aufzubauen. Dieser Switch war eine der lehrreichsten und befriedigendsten erfahrungen meiner bisherigen Entwicklerlaufbahn. Moderne Webtechnologien sind etwas tolles 💞',
+      'Mein Herzensprojekt. Mit meinem Vater und meinem Bruder habe ich Cleaner.Earth, eine Online Plattform zur Finanzierung von Plastiksammel- und Recyclingprojekten, gegründet und die Entwicklung der Website übernommen. Nachdem wir zunächst mit Wordpress arbeiteten haben wir uns dazu entschieden die Website als React App mit Rust backend aufzubauen. Dieser Switch war eine der lehrreichsten und befriedigendsten erfahrungen meiner bisherigen Entwicklerlaufbahn. Moderne Webtechnologien sind etwas wunderschönes 💞',
     interval: {
       start: new Date('2019-3-1'),
       end: new Date(),
@@ -63,12 +63,12 @@ const occupationTimeLine = [
     name: 'Barista',
     organisation: 'Unser Cafe/ Portuguese Bakery by Veloso & Freshlife GmbH',
     description:
-      'Ein weiterer Caféjob. Zahlose Kaffees gekocht, super viele Nette Menschen kennengelernt, aber früher oder später wird es wohl Zeit sich von der Gastronomie zu verabschieden. Allerdings kann ich jetzt exzellenten Kaffee kochen.',
+      'Ein weiterer Caféjob. Zahlose Tassen Kaffee gekocht und super viele Nette Menschen kennengelernt. Früher oder später wird es wohl allerdings Zeit sich von der Gastronomie zu verabschieden. Allerdings kann ich jetzt exzellenten Kaffee kochen.',
     interval: {
       start: new Date('2019-5-1'),
       end: new Date('2020-10-18'),
     },
-    color: '#0d0d0d',
+    color: '#2d2f2f',
     textColor: '#EEB',
   },
 ].reverse();
